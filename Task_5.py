11533b8e61a16ae1ca81f10802395567314a22bc
Rairakku <rairakkuaki@gmail.com>
2025-10-30 01:08:29 +0300

    Add files via upload

---
+++ Task_5.py
@@ -0,0 +1,9 @@
+shopping_list = ['молоко', 'хлеб', 'яйца']
+shopping_list.append('сахар')
+sum_shopping_list = len(shopping_list)
+print(shopping_list)
+print('Сколько всего продуктов в списке:', sum_shopping_list)
+
+unique_items = {'молоко', 'хлеб', 'яйца'}
+unique_items.add('сахар')
+print(unique_items)
